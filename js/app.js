e36a67d48971275c1533d409cbc0f273a362b201
JON STILLSON <JCSTILLSON77@GMAIL.COM>
2023-05-09 13:26:55 -0400

    app.js changes

--- js/app.js
+++ js/app.js
@@ -30,38 +30,50 @@ let hours = ['6am', '7am', '8am','9am','10am','11am','12pm','1pm','2pm','3pm','4
 let storeSection = document.getElementById('stores');
 
 let seattle = {
-  location: 'Seattle',
-  minCustomer: '23',
-  maxCustomer: '65',
-  avgSale: '6.3',
-  numCustomer: '0', //will hold a random number of customers
-  randNumCust: function (min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  },
-  getNum: function () {
-    this.numCustomer = this.randNumCust(this.minCustomer, this.maxCustomer); 
-    return this.numCustomer; //unnecessary?
-  }
+    location: 'Seattle',
+    minCustomer: '23',
+    maxCustomer: '65',
+    avgSale: '6.3',
+    numCustomer: '0', //will hold a random number of customers
+    randNumCust: function (min, max){
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    },
+    getNum: function () {
+        this.numCustomer = this.randNumCust(this.minCustomer, this.maxCustomer); 
+        return this.numCustomer; //unnecessary?
+    },
 
-  cookiePurchase: [],
-  totalCookies: 0,
+    cookiePurchase: [],
+    totalCookies: 0,
 
-  //calculate cookies and the DOM via render
-  render: function () {
+    //calculate cookies and the DOM via render
+    render: function () {
     for (let i = 0; i < hours.length; i++){
         let cookiesBought = Math.ceil(this.avgSale * this.getNum()); //round the numbers off.
         console.log(cookiesBought);
         this.totalCookies += cookiesBought; //ading the cookies bought to the total per iteration.
 
         this.cookiePurchase.push(cookiesBought); //adds to the end of the array
-    }
+        }
+    
+        console.log(this.cookiePurchase);
+        console.log('TotalSales:', this.totalCookies);
+
+        /***DOM Manipulation */
+        let storeHeading = document.createElement('h2');
+        storeHeading.textContent = this.location;
+        storeSection.appendChild(storeHeading);
+
+        let storeList = document.createElement('ul');
+        storeSection.appendChild(storeList);
+
+        for(let i = 0; i < this.cookiePurchase.length; i++){
+            let cookieItem = document.createElement('li');
+            cookieItem.textContent = `${hours[i]}: ${this.cookiePurchase[i]} cookies`;
+            storeList.appendChild(cookieItem);
+        }
+    let cookieTotal = document.createElement('li');
+    cookieTotal.textContent = `TotalSales: ${this.totalCookies}`;
+    storeList.appendChild(cookieTotal); 
   }
-  console.log(this.cookiePurchase);
-
-  console.log('TotalSales:', this.totalCookies);
-
-  /***DOM Manipulation */
-  let storeHeading = document.createElement('h2');
-  storeHeading.innerText = this.location;
-
 }
